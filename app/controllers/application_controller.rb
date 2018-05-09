702e10ab138205c1b6c225ba469b4327524af4f9
Stephanie Fischler <sjfischler@gmail.com>
2018-05-09 16:07:36 -0400

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,4 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized
-    flash[:warning] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
-  end
 end
